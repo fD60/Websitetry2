d3a993651cb54f2c4929dfbe153f06c358c7c58d
Flagot Demere <Flagot746@gmail.com>
2023-07-12 23:10:06 +0100

    i made changes to it

---
+++ src/helpers/KidsList.js
@@ -0,0 +1,22 @@
+import item1 from "../assets/kids1.PNG"
+import item2 from "../assets/Kids2.PNG"
+import item3 from "../assets/Kids3.PNG"
+
+
+export const KidsList = [
+    {
+     name: "BURBERRY",
+     image: item1,
+     price: 15.99,
+   },
+   {
+     name: "GUCCI",
+     image: item2,
+     price: 51.99,
+   },
+   {
+     name: "GIVENCHY",
+     image: item3,
+     price: 59.99,
+   },
+]
--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -1,4 +1,6 @@
 import item1 from "../assets/Kids1.PNG"
+import item2 from "../assets/Kids2.PNG"
+import item3 from "../assets/Kids3.PNG"
 import Pepperoni from "../assets/pepperoni.jpg";
 import Margherita from "../assets/margherita.jpg";
 import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
@@ -11,6 +13,16 @@ export const MenuList = [
     name: "BURBERRY",
     image: item1,
     price: 15.99,
+  },
+  {
+    name: "GUCCI",
+    image: item2,
+    price: 51.99,
+  },
+  {
+    name: "GIVENCHY",
+    image: item3,
+    price: 59.99,
   },
   {
     name: "Pepperoni Pizza",
